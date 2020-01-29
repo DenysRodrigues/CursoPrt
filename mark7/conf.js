2811ab67964056493cc212cf590ba4be84f22b41
Denys Rodrigues <denysrodrigues5@hotmail.com>
2020-01-29 18:20:56 -0300

    Atualizando

--- mark7/conf.js
+++ mark7/conf.js
@@ -22,6 +22,17 @@ exports.config = {
             fixedScreenshotName: true
         }))
 
+        var AllureReporter =  require('jasmine-allure-reporter')
+
+        jasmine.getEnv().addReporter(new AllureReporter());
+        jasmine.getEnv().afterEach(function(done){
+            browser.takeScreenshot().then(function(png){
+                allure.createAttachment('Screenshot', function(){
+                    return new Buffer(png, 'base64');
+                }, 'image/png')();
+                done();
+            })
+        })
 
         var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
         jasmine.getEnv().addReporter(new SpecReporter({
